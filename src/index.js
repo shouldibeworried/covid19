62778b7e6b3e65e69cb95600898bab2fd3599489
Felix Distel <felix.distel@tu-dresden.de>
2020-05-30 20:15:56 -0700

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+/* Put any other imports below so that CSS from your
+ * components takes precedence over default styles. */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
