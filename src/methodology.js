915bb2e0e5d538a23f7f0b001a1b58b357e878f4
Felix Distel <felix.distel@tu-dresden.de>
2020-06-08 21:20:12 -0700

    linting

--- src/methodology.js
+++ src/methodology.js
@@ -1,38 +1,45 @@
-const avgDays = 7,
-      genLen = 6,
-      infectionFatalityMin = 0.003,
-      infectionFatalityMax = 0.013,
-      infectionFatalityMed = 0.008,
+const avgDays = 7;
+const genLen = 6;
+const infectionFatalityMin = 0.003;
+const infectionFatalityMax = 0.013;
+const infectionFatalityMed = 0.008;
 
-      // TODO increase when we have more data
-      deathOffset = 14,
+// TODO increase when we have more data
+const deathOffset = 14;
 
-      geoMean = (sequence) => Math.pow(sequence.reduce((x1, x2) => x1 * x2),
-                                       1 / sequence.length),
+const geoMean = (sequence) => (
+  sequence.reduce((x1, x2) => x1 * x2) ** (1 / sequence.length)
+);
 
-      rNoughtSequence = (cases) => {
+const rNoughtSequence = (cases) => {
+  /* Calculate rNought by dividing new cases in current period
+   * by new cases in preceding period */
 
-          /* Calculate rNought by dividing new cases in current period
-           * by new cases in preceding period */
+  const rNought = (total, index) => (
+    (total - cases[index - genLen])
+    / (cases[index - genLen]
+      - cases[index - (2 * genLen)])
+  );
 
-          const rNought = (total, index) => (
-              (total - cases[index - genLen]) /
-              (cases[index - genLen] -
-               cases[index - (2 * genLen)])
-          );
+  return cases.map(rNought).slice(2 * genLen);
+};
 
-          return cases.map(rNought).slice(2 * genLen);
-      },
-      caseFatality = (cases, deaths) => {
+const caseFatality = (cases, deaths) => {
+  /* Ratio of confirmed cases who passed after
+   * deathOffset days. */
 
-          /* Ratio of confirmed cases who passed after
-           * deathOffset days. */
+  const nCases = (
+    cases[cases.length - 1 - deathOffset]
+    - cases[cases.length - 1 - 2 * deathOffset]
+  );
+  const nDeaths = (
+    deaths[cases.length - 1] - deaths[cases.length - 1 - deathOffset]
+  );
 
-          let nCases = (cases[cases.length - 1 - deathOffset] -
-                        cases[cases.length - 1 - 2 * deathOffset])
-          let nDeaths = (deaths[cases.length - 1] -
-                     deaths[cases.length - 1 - deathOffset])
-          return nDeaths / nCases
-      };
+  return nDeaths / nCases;
+};
 
-export { rNoughtSequence, geoMean, avgDays, caseFatality, infectionFatalityMin, infectionFatalityMax, infectionFatalityMed  };
+export {
+  rNoughtSequence, geoMean, avgDays, caseFatality, infectionFatalityMin,
+  infectionFatalityMax, infectionFatalityMed,
+};
