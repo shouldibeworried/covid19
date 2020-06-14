e56537044ffe16bed3c1285aa528ccd69145d5c5
Felix Distel <felix.distel@tu-dresden.de>
2020-06-14 13:38:52 -0700

    Minor fixes

--- src/thresholds.js
+++ src/thresholds.js
@@ -1,24 +1,24 @@
 const r0AlertLevels = {
-  0.95: 'info',
-  1.05: 'danger',
+  0.95: 'success',
+  1.05: 'info',
 };
-const defaultR0AlertLevel = 'success';
+const defaultR0AlertLevel = 'danger';
 
 const projectionPhrases = {
-  0.5: 'a smaller',
-  0.9: 'about the same',
-  1.1: 'a greater',
-  2: 'more than double that',
-  3: 'more than three times that',
-  5: 'more than five times that',
-  10: 'more than ten times that',
-  20: 'more than twenty times that',
+  0.5: 'less than half that',
+  0.9: 'a smaller',
+  1.1: 'about the same',
+  2: 'a greater',
+  3: 'more than double that',
+  5: 'more than three times that',
+  10: 'more than five times that',
+  20: 'more than ten times that',
 };
-const defaultProjectionPhrase = 'less than half that';
+const defaultProjectionPhrase = 'more than twenty times that';
 
 const checkLevel = (levels, defaultValue) => (
   (v) => {
-    const l = Object.keys(levels).find((level) => level < v);
+    const l = Object.keys(levels).find((level) => level > v);
     return typeof l === 'undefined' ? defaultValue : levels[l];
   }
 );
