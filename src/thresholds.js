0ed624c250be8d13511c8463096a826480566c16
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-06-14 14:01:24 -0700

    Use Map instead of objects

--- src/thresholds.js
+++ src/thresholds.js
@@ -1,25 +1,25 @@
-const r0AlertLevels = {
-  0.95: 'success',
-  1.05: 'info',
-};
+const r0AlertLevels = new Map([
+  [0.95, 'success'],
+  [1.05, 'info'],
+]);
 const defaultR0AlertLevel = 'danger';
 
-const projectionPhrases = {
-  0.5: 'less than half that',
-  0.9: 'a smaller',
-  1.1: 'about the same',
-  2: 'a greater',
-  3: 'more than double that',
-  5: 'more than three times that',
-  10: 'more than five times that',
-  20: 'more than ten times that',
-};
+const projectionPhrases = new Map([
+  [0.5, 'less than half that'],
+  [0.9, 'a smaller'],
+  [1.1, 'about the same'],
+  [2, 'a greater'],
+  [3, 'more than double that'],
+  [5, 'more than three times that'],
+  [10, 'more than five times that'],
+  [20, 'more than ten times that'],
+]);
 const defaultProjectionPhrase = 'more than twenty times that';
 
 const checkLevel = (levels, defaultValue) => (
   (v) => {
-    const l = Object.keys(levels).find((level) => level > v);
-    return typeof l === 'undefined' ? defaultValue : levels[l];
+    const l = Array.from(levels.keys()).find((level) => level > v);
+    return typeof l === 'undefined' ? defaultValue : levels.get(l);
   }
 );
 
