49eddf8f5fdf057969a9d895f197c939fc3410cf
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-06-16 17:39:12 -0700

    Make country state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.css';
 
 import CovidApp from './components';
 
-import eu from './data/eu.json';
-import population from './data/population.json';
-
 
 function App() {
-  const props = {
-    country: 'United Kingdom',
-    cases: eu.cases,
-    deaths: eu.deaths,
-    population,
-  };
-  return CovidApp(props);
+  return new CovidApp();
 }
 
 export default App;
