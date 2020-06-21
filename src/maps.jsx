afa1fad0ffeb01b84ed5ead8db5c06890163c984
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-06-20 18:40:11 -0700

    add legends

--- src/maps.jsx
+++ src/maps.jsx
@@ -8,8 +8,10 @@ import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import { rNoughtWeeklyAverage, confirmedRecentCasesPer100K } from './methodology';
 import {
   r0Color,
+  r0Colors,
   defaultR0Color,
   confirmedCasesColor,
+  confirmedCasesColors,
   defaultConfirmedCasesColor,
 } from './thresholds';
 
@@ -72,6 +74,54 @@ Map.propTypes = {
 };
 
 
+const LegendEntry = (props) => {
+  const { key, text, color } = props;
+  return (
+    <span key={key} style={{ paddingRight: '10px', display: 'inline-block' }}>
+      <svg
+        className="bi bi-square-fill"
+        width="1em"
+        height="1em"
+        viewBox="0 0 16 16"
+        fill={color}
+        stroke="grey"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="16" height="16" rx="2" />
+      </svg>
+      {text}
+    </span>
+  );
+};
+
+LegendEntry.propTypes = {
+  key: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+const Legend = (props) => {
+  const { colorMap } = props;
+  const colorArray = Array.from(colorMap);
+  return (
+    <div>
+      {colorArray.slice(0, -1).map(([key, value]) => (
+        <LegendEntry key={key} text={` < ${key}`} color={value} />
+      ))}
+      <LegendEntry
+        key={colorArray.slice(-1)[0][0]}
+        text={` \u{2265} ${colorArray.slice(-2)[0][0]}`}
+        color={colorArray.slice(-1)[0][1]}
+      />
+    </div>
+  );
+};
+
+Legend.propTypes = {
+  colorMap: PropTypes.instanceOf(Map).isRequired,
+};
+
+
 export const R0Map = (props) => {
   const { mapType, cases } = props;
   const colorMap = colorMapFactory(
@@ -88,6 +138,7 @@ export const R0Map = (props) => {
           </Card.Title>
           <Card.Text>
             <Map mapType={mapType} colorMap={colorMap} />
+            <Legend colorMap={r0Colors} />
           </Card.Text>
         </Card.Body>
       </Card>
@@ -96,14 +147,7 @@ export const R0Map = (props) => {
 };
 
 R0Map.propTypes = {
-  mapType: PropTypes.shape({
-    geoUrl: PropTypes.string,
-    projection: PropTypes.string,
-    projectionConfig: PropTypes.shape({
-      rotate: PropTypes.arrayOf(PropTypes.number),
-      scale: PropTypes.number,
-    }),
-  }).isRequired,
+  mapType: propTypeMap.isRequired,
   cases: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
 };
 
@@ -123,6 +167,7 @@ export const RecentCasesMap = (props) => {
           </Card.Title>
           <Card.Text>
             <Map mapType={mapType} colorMap={colorMap} />
+            <Legend colorMap={confirmedCasesColors} />
           </Card.Text>
         </Card.Body>
       </Card>
@@ -131,14 +176,7 @@ export const RecentCasesMap = (props) => {
 };
 
 RecentCasesMap.propTypes = {
-  mapType: PropTypes.shape({
-    geoUrl: PropTypes.string,
-    projection: PropTypes.string,
-    projectionConfig: PropTypes.shape({
-      rotate: PropTypes.arrayOf(PropTypes.number),
-      scale: PropTypes.number,
-    }),
-  }).isRequired,
+  mapType: propTypeMap.isRequired,
   cases: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
   population: PropTypes.objectOf(PropTypes.number).isRequired,
 };
