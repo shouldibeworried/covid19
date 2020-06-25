4e0438c8c2131d1a023029775e92fbd33245eb4d
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-06-24 20:56:00 -0700

    Make the color scheme match

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
