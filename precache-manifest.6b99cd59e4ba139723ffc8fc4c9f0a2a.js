d84922ce035c1bf7b4f83c05f249240971df6622
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-06-29 22:47:06 -0700

    Updates

---
+++ precache-manifest.6b99cd59e4ba139723ffc8fc4c9f0a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1a401d48393166d9370744bec4082e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b95e6b9303eb7be683ba",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "11b0193be20e4d55058f",
+    "url": "/static/css/main.050ba659.chunk.css"
+  },
+  {
+    "revision": "b95e6b9303eb7be683ba",
+    "url": "/static/js/2.f5d00e32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f5d00e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b0193be20e4d55058f",
+    "url": "/static/js/main.91c1c176.chunk.js"
+  },
+  {
+    "revision": "4128aab56c1267f4e961",
+    "url": "/static/js/runtime-main.5d44d82b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19/precache-manifest.a0984ab564b466ee894809745045a141.js"
+  "/precache-manifest.6b99cd59e4ba139723ffc8fc4c9f0a2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
