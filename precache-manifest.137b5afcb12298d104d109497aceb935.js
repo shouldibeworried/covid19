c8ca1d6d470ff508dffa01f9ef5c8a444f7127fd
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-10-19 23:54:20 -0700

    Updates

---
+++ precache-manifest.137b5afcb12298d104d109497aceb935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44547221490e23c896db50db6f6378b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5323151d7fd226e10dcb",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6c62048567bc904ca168",
+    "url": "/static/css/main.050ba659.chunk.css"
+  },
+  {
+    "revision": "5323151d7fd226e10dcb",
+    "url": "/static/js/2.99497091.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.99497091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c62048567bc904ca168",
+    "url": "/static/js/main.eeb49555.chunk.js"
+  },
+  {
+    "revision": "4128aab56c1267f4e961",
+    "url": "/static/js/runtime-main.5d44d82b.js"
+  }
+]);
