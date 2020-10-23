17f609265a949398821d88e00719686cf6b49391
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-10-23 11:58:58 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19/precache-manifest.ddad271f817ebbc0dc217d47e90cb02b.js"
+  "/covid19/precache-manifest.5019c5d87a7df1b69170b14f14c1bb51.js"
 );
 
 self.addEventListener('message', (event) => {
