e10ea863b1492533c5d4504e45b2e943a8d9b90b
ShouldIBeWorried19 <shouldibeworried@gmx.net>
2020-10-25 11:33:31 -0700

    Updates

---
+++ precache-manifest.88264d5fba9516f11137dda4c1c6cdab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f00d446a5fdfcbf8b3d58b5b350f362",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "5323151d7fd226e10dcb",
+    "url": "/covid19/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "be0564f2c9d15ae5b0cc",
+    "url": "/covid19/static/css/main.050ba659.chunk.css"
+  },
+  {
+    "revision": "5323151d7fd226e10dcb",
+    "url": "/covid19/static/js/2.99497091.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19/static/js/2.99497091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0564f2c9d15ae5b0cc",
+    "url": "/covid19/static/js/main.6fcb2de7.chunk.js"
+  },
+  {
+    "revision": "3f9ea9de9b45ec0fb219",
+    "url": "/covid19/static/js/runtime-main.59095949.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66354e26a9039d30db206675e872cb78.js"
+  "/covid19/precache-manifest.88264d5fba9516f11137dda4c1c6cdab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
